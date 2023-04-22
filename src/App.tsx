32f43a76a9c0e171c69c362027d0f7f7729d93b9
tkonishi <asdyuy23@gmail.com>
2023-04-22 12:01:35 +0900

    ・生成、クリアボタン追加

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import "./App.css";
 import QRCode from "react-qr-code";
 import { QR25D, QRResImage, QRBubble, QRDsj, QRNormal } from "react-qrbtf";
 import { encodeData } from "react-qrbtf";
+import { Button } from "react-bootstrap";
 
 type Memotype = {
   url: string;
@@ -10,7 +11,18 @@ type Memotype = {
 
 export const App = () => {
   const [urlLink, setUrlLink] = useState<Memotype>({ url: "" });
+  const [visible, setVisible] = useState<boolean>(false);
   const qrcode = encodeData({ text: "react-qrbtf" });
+
+  const generateQrCode = () => {
+    setVisible(true);
+  };
+
+  const clearQrCode = () => {
+    setVisible(false);
+    setUrlLink({ url: "" });
+  };
+
   return (
     <div>
       <h1>QRコード生成</h1>
@@ -19,17 +31,18 @@ export const App = () => {
         value={urlLink.url}
         onChange={(event) => setUrlLink({ url: event.target.value })}
       ></input>
-      {urlLink.url}
-      <button>生成</button>
+      <button onClick={generateQrCode}>生成</button>
+      <button onClick={clearQrCode}>クリア</button>
       <div
         style={{
           height: "auto",
           margin: "0 auto",
-          maxWidth: 64,
+          maxWidth: 200,
           width: "100%",
+          visibility: visible ? "visible" : "hidden",
         }}
       >
-        <QRCode
+        {/* <QRCode
           size={256}
           style={{ height: "auto", maxWidth: "100%", width: "100%" }}
           value={urlLink.url}
@@ -37,7 +50,7 @@ export const App = () => {
         />
         <QRNormal qrcode={qrcode} />
         <QRDsj qrcode={qrcode} />
-        <QRBubble qrcode={qrcode} />
+        <QRBubble qrcode={qrcode} /> */}
         <QR25D value={urlLink.url} />
         {/* <QRResImage qrcode={qrcode} image="./img/nihonchizu-hakuchizu.png" /> */}
       </div>
